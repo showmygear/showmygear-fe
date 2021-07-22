af52d1b33d98faba087e72ede2dc21dab8323394
ruoshin <ruoshin0706@gmail.com>
2021-07-23 02:06:36 +0800

    Ability to toggle navigation

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import LogoImg from 'images/logo.svg';
 
 const ShowMyGear = () => {
   const [active, setActive] = useState(0);
+  const [isOpen, setOpen] = useState(0);
 
   return (
     <div className='layout'>
-      <Navigation userName='鹿野忠雄'/>
-      <div className='logo'>
-        <a href='/'>
-          <img src={LogoImg} alt='ShowMyGear' />
-        </a>
+
+      <Navigation
+        userName='鹿野忠雄'
+        isOpen={isOpen}
+        onClick={setActive}/>
+      <div className='logo' onClick={()=>setOpen(isOpen=>!isOpen)}>
+        <img src={LogoImg} alt='ShowMyGear' />
       </div>
       {
         active == 1 ? <ProfileIndex /> : <TrailIndex onClick={setActive} />
--- src/components/organisms/Navigation.js
+++ src/components/organisms/Navigation.js
@@ -2,13 +2,14 @@ import Button from 'components/atoms/Button'
 
 const Navigation = ({
   userName,
-
+  onClick,
+  isOpen
 }) => {
   return (
-    <div className='navigation'>
+    <div className={`navigation ${isOpen ? 'open' : ''}`}>
       <div className='navigation-header'>{userName}</div>
       <div className='navigation-body'>
-        <Button variant='default' fullWidth>新增檢查表</Button>
+        <Button variant='default' fullWidth onClick={()=>onClick(2)}>新增檢查表</Button>
         <Button variant='default' fullWidth>新增裝備</Button>
       </div>
       <div className='navigation-footer'>
